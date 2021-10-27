576ed6267c86b9a7a872994c347c5af86303a276
aachaso6 <asheludykova@gmail.com>
2021-10-27 21:01:34 +0300

    homework 2 edited. data from the site superjob.ru has been added
    Урок 2. Парсинг HTML. BeautifulSoup, MongoDB

--- homework_2/lesson_2_hw1.py
+++ homework_2/lesson_2_hw1.py
@@ -14,22 +14,29 @@ import requests
 from bs4 import BeautifulSoup as bs
 from pprint import pprint
 import pandas as pd
-import json
 
 
-url = 'https://www.hh.ru'
+
+url_hh = 'https://www.hh.ru'
+url_sj = 'https://www.superjob.ru'
 user_text = input("Enter the direction of work: ")
 
-params = {'area': 1,
+params_hh = {'area': 1,
           'fromSearchLine': 'true',
           'text': user_text,
           'page': 0}
+
+params_sj = {'keywords': user_text,
+             'noGeo': 1,
+             'page': 1}
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'}
 
+
 vacancy_list = []
 while True:
-    response = requests.get(url + '/search/vacancy', params=params, headers=headers)
+    response = requests.get(url_hh + '/search/vacancy', params=params_hh, headers=headers)
     dom = bs(response.text, 'html.parser')
     vacancies = dom.find_all('div', {'class': 'vacancy-serp-item'})
 
@@ -69,8 +76,59 @@ while True:
             vacancy_data['salary_max'] = salary_max
             vacancy_data['salary_currency'] = salary_currency
             vacancy_list.append(vacancy_data)
-        print(f"Обработана {params['page']} страница")
-        params['page'] += 1
+        print(f"Обработана {params_hh['page']+1} страница с {url_hh}")
+        params_hh['page'] += 1
+    else:
+        break
+
+while True:
+    response = requests.get(url_sj + '/vacancy/search/', params=params_sj, headers=headers)
+    dom = bs(response.text, 'html.parser')
+    vacancies = dom.find_all('div', {'class': 'f-test-search-result-item'})
+
+    if response.ok and vacancies:
+        for vacancy in vacancies:
+            vacancy_data = {}
+            try:
+                info = vacancy.find('a')
+                name = info.text
+                link = url_sj + info['href']
+                company_name = vacancy.find('span', {'class': 'f-test-text-vacancy-item-company-name'}).text
+            except:
+                pass
+
+            try:
+                salary = vacancy.find('span', {'class': 'f-test-text-company-item-salary'}).text.split()
+                if salary[0] == "По":
+                    salary_min = None
+                    salary_max = None
+                    salary_currency = None
+                elif salary[0] == "до":
+                    salary_min = None
+                    salary_max = int(salary[1] + salary[2])
+                    salary_currency = salary[3][0:4]
+                elif salary[0] == "от":
+                    salary_min = int(salary[1] + salary[2])
+                    salary_max = None
+                    salary_currency = salary[3][0:4]
+                else:
+                    salary_min = int(salary[0] + salary[1])
+                    salary_max = int(salary[3] + salary[4])
+                    salary_currency = salary[5][0:4]
+            except:
+                pass
+
+
+            vacancy_data['name'] = name
+            vacancy_data['link'] = link
+            vacancy_data['company_name'] = company_name
+            vacancy_data['site'] = 'superjob.ru'
+            vacancy_data['salary_min'] = salary_min
+            vacancy_data['salary_max'] = salary_max
+            vacancy_data['salary_currency'] = salary_currency
+            vacancy_list.append(vacancy_data)
+        print(f"Обработана {params_sj['page']} страница с {url_sj}")
+        params_sj['page'] += 1
     else:
         break
 
@@ -78,6 +136,6 @@ while True:
 df = pd.DataFrame(vacancy_list)
 print(df.sample(n=5))
 
-df.to_csv("data.csv", sep=";")
+df.to_csv("data.csv", sep=";", encoding="utf-8")
 
 
